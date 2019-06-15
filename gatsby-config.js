2e880291b1ed428b6263f37f072df615e12eae4f
drood87 <dbernstein1987@hotmail.com>
2019-06-15 10:20:03 +0800

    install styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-eslint',
+    'gatsby-plugin-styled-components',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
